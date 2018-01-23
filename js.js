3cab431d65e6f01b4229728127cdce4b227fbf9f
XA <18772945827@163.com>
2018-01-23 11:22:13 +0800

    add js

---
+++ js.js
@@ -0,0 +1,54 @@
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+
+var contain = document.getElementById("contain");
+var list = contain.children;//contain的所有element子元素，即9个格子
+var length = list.length-1;
+
+var a=1;
+start.addEventListener("click",startL);
+stop.addEventListener("click",stopL);
+
+
+
+var times;
+//开始闪烁事件
+function startL(){
+	times = window.setInterval("Light(list,length)",1000);
+}
+//停止闪烁事件
+function stopL(){
+	window.clearInterval(times);
+	for(var i=0;i<length;i++){
+		var div = list.item(i);
+		div.style.background = "rgb(255,166,0)";
+	}
+}
+
+var divArr = new Array();//存储被选中的div
+//随机选中三个div设置颜色
+function Light(list1,length1){
+	while(divArr.length!=0){
+		divArr.pop().style.background="rgb(255,166,0)";
+	}
+	for(var i=1;i<4;i++){
+		var div = list1.item(number(length1));
+		div.style.background = color();
+		divArr.push(div);
+	}
+}
+
+//生成随机颜色
+function color(){
+	var red = Math.random() * 255;
+	var green = Math.random() * 255;
+	var blue = Math.random() * 255;
+	var color="rgb("+Math.floor(red)+","+Math.floor(green)+","+Math.floor(blue)+")";
+	return color;
+}
+
+//生成随机数字
+function number(length){
+	var i = Math.random() * length;
+	return i;
+}
